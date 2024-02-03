4fdf4ae8eb832962799af31df3a2e41b74d6ae11
clhsieh <clhsieh@synology.com>
2024-02-03 23:36:16 +0800

    修正缺少 php 啟動 script 檔案

---
+++ start.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+set -e
+
+type=${CONTAINER_TYPE:-app}
+
+if [ "$type" = "app" ]; then
+    exec php-fpm
+elif [ "$type" = "dividend" ]; then
+    echo "Running the queue: dividend"
+    php artisan queue:work --verbose --queue=get:dividend --sleep=300 --tries=0
+elif [ "$type" = "emails" ]; then
+    echo "Running the queue: emails"
+    php artisan queue:work --verbose --queue=emails --sleep=10 --tries=1
+elif [ "$type" = "simulation" ]; then
+    echo "Running the queue: simulation"
+    php artisan queue:work --verbose --queue=simulation --sleep=10 --tries=0
+elif [ "$type" = "websocket" ]; then
+    echo "Running the queue: websocket"
+    php artisan queue:work redis --verbose --queue=websocket
+elif [ "$type" = "scheduler" ]; then
+    while [ true ]
+    do
+      php artisan schedule:run --verbose --no-interaction &
+      sleep 60
+    done
+else
+    echo "Could not match the PHP container type \"type\""
+    exit 1
+fi
